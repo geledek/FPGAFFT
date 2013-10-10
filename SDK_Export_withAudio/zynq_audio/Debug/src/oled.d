5365b14d8d40384f3f698c035037774860337ab7
#XU SHIPENG# <SXU008@e.ntu.edu.sg>
2013-10-10 10:40:33 +0800

    Hardware with audio

---
+++ SDK_Export_withAudio/zynq_audio/Debug/src/oled.d
@@ -0,0 +1,33 @@
+src/oled.d: ../src/oled.c ../src/oled.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/font.h
+
+../src/oled.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/font.h:
---
+++ SDK_Export_withAudio/zynq_audio/Debug/src/zynq_audio.d
@@ -0,0 +1,38 @@
+src/zynq_audio.d: ../src/zynq_audio.c ../src/audio.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/oled.h ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../zynq_audio_bsp/ps7_cortexa9_0/include/sleep.h
+
+../src/audio.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/oled.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../zynq_audio_bsp/ps7_cortexa9_0/include/sleep.h:
