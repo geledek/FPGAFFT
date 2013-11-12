544132c097983553730ca1e38b99a8f98f0f5c00
#XU SHIPENG# <SXU008@e.ntu.edu.sg>
2013-11-12 22:39:56 +0800

    clean

--- Xilinx/FFT/Debug/src/kiss_fft130/kiss_fft.d
+++
@@ -1,6 +0,0 @@
-src/kiss_fft130/kiss_fft.d: ../src/kiss_fft130/kiss_fft.c \
- ../src/kiss_fft130/_kiss_fft_guts.h ../src/kiss_fft130/kiss_fft.h
-
-../src/kiss_fft130/_kiss_fft_guts.h:
-
-../src/kiss_fft130/kiss_fft.h:
